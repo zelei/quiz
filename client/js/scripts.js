4b98a0847ced18a5f364a92f536fa23e67be89a2
Sandor Zelei <sandor.zelei@gmail.com>
2014-02-16 15:35:59 -0500

    Minor changes

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,76 +1,90 @@
+var angular = angular || {};
+
 var quizApp = angular.module('quizApp', []);
  
 quizApp.controller('QuizCtrl', function ($scope) {
   
-  $scope.TYPE = {
-    G : {},
-    I : {},
-    GI : {},
-    BI : {}
-  };
-  
-  $scope.score = 0;
-  
-  $scope.isFinished = false;
-  
-  var index = 0;
-  
-  var words = [
-    {'question': 'anticipate', 'solution' : $scope.TYPE.G},
-    {'question': 'anticipate2', 'solution' : $scope.TYPE.I},
-    {'question': 'anticipate3', 'solution' : $scope.TYPE.GI},
-    {'question': 'anticipate4', 'solution' : $scope.TYPE.BI},
-    {'question': 'anticipate', 'solution' : $scope.TYPE.G},
-    {'question': 'anticipate2', 'solution' : $scope.TYPE.I},
-    {'question': 'anticipate3', 'solution' : $scope.TYPE.GI},
-    {'question': 'anticipate4', 'solution' : $scope.TYPE.BI},
-    {'question': 'anticipate', 'solution' : $scope.TYPE.G},
-    {'question': 'anticipate2', 'solution' : $scope.TYPE.I},
-    {'question': 'anticipate3', 'solution' : $scope.TYPE.GI},
-    {'question': 'anticipate4', 'solution' : $scope.TYPE.BI},
-    {'question': 'anticipate5', 'solution' : $scope.TYPE.G}
-  ];
-  
-  $scope.setAnswer = function(solution) {
-  
-    if(isGoodAnswer(solution)) {
-        $scope.score+=1;
-    }
+    $scope.TYPE = {
+        G : {}, I : {}, GI : {}, BI : {}
+    };
     
-    if(! ($scope.getIndex() == $scope.getWordCount())) {
-       increaseIndex();
-    } else {
-      $scope.isFinished = true;  
-    }
+    $scope.score = 0;
     
-  };
-  
-  $scope.getCurrentWord = function() {
-    return words[index].question;
-  };
-  
-  $scope.getIndex = function() {
-    return index + 1;
-  };
-  
-  $scope.getWordCount = function() {
-    return words.length;
-  };
-  
-  $scope.getPercent = function() {
-    return (100 / $scope.getWordCount()) * $scope.getIndex();
-  };
+    $scope.isFinished = false;
+    
+    $scope.currentWord = "";
+    
+    var index = 0;
+    
+    var words = [
+        {'question': 'anticipate', 'solution' : $scope.TYPE.G},
+        {'question': 'anticipate2', 'solution' : $scope.TYPE.I},
+        {'question': 'anticipate3', 'solution' : $scope.TYPE.GI},
+        {'question': 'anticipate4', 'solution' : $scope.TYPE.BI},
+        {'question': 'anticipate', 'solution' : $scope.TYPE.G},
+        {'question': 'anticipate2', 'solution' : $scope.TYPE.I},
+        {'question': 'anticipate3', 'solution' : $scope.TYPE.GI},
+        {'question': 'anticipate4', 'solution' : $scope.TYPE.BI},
+        {'question': 'anticipate', 'solution' : $scope.TYPE.G},
+        {'question': 'anticipate2', 'solution' : $scope.TYPE.I},
+        {'question': 'anticipate3', 'solution' : $scope.TYPE.GI},
+        {'question': 'anticipate4', 'solution' : $scope.TYPE.BI},
+        {'question': 'anticipate5', 'solution' : $scope.TYPE.G}
+    ];
+    
+    $scope.setAnswer = function(solution) {
+    
+        if(isGoodAnswer(solution)) {
+            increaseScore();
+        }
+        
+        if((getIndex() !== getWordCount())) {
+           increaseIndex();
+           setCurrentWord();
+           setPercent(getWordCount(), getIndex());
+        } else {
+          $scope.isFinished = true;  
+        }
+    
+    };
+    
+    $scope.percent = function() {
+        return (100 / getWordCount()) * getIndex();
+    };
+    
+    var getIndex = function() {
+        return index + 1;
+    };
+    
+    var setPercent = function(wordCount, index) {
+        $scope.percent = (100 / wordCount) * index;
+    };
+    
+    var setCurrentWord = function() {
+        $scope.currentWord = words[index].question;
+    };
+    
+    var getWordCount = function() {
+        return words.length;
+    };
+    
+    var getCurrentQuestion = function() {
+        return words[index];
+    };
+    
+    var isGoodAnswer = function(solution) {
+        return getCurrentQuestion().solution === solution;
+    };
+    
+    var increaseIndex = function() {
+        index += 1;
+    };
 
-  getCurrentQuestion = function() {
-    return words[index];
-  };
-  
-  isGoodAnswer = function(solution) {
-    return getCurrentQuestion().solution === solution;
-  }
-  
-  increaseIndex = function() {
-    index = index + 1;
-  }
+    var increaseScore = function() {
+        $scope.score+=1;
+    };
+
+    setCurrentWord();
+    setPercent(getWordCount(), getIndex());
 
 });
